20ef16af3500a6994ce34b33f7a092cee409afb4
Phan Trung Thanh <trung.thanh.phan@canonical.com>
2025-06-27 12:12:44 +0200

    update lib

--- lib/charms/haproxy/v0/haproxy_route.py
+++ lib/charms/haproxy/v0/haproxy_route.py
@@ -143,7 +143,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 5
+LIBPATCH = 6
 
 logger = logging.getLogger(__name__)
 HAPROXY_ROUTE_RELATION_NAME = "haproxy-route"
@@ -994,6 +994,7 @@ class HaproxyRouteRequirer(Object):
         connect_timeout: int = 60,
         queue_timeout: int = 60,
         server_maxconn: Optional[int] = None,
+        unit_address: Optional[str] = None,
     ) -> None:
         """Update haproxy-route requirements data in the relation.
 
@@ -1025,7 +1026,9 @@ class HaproxyRouteRequirer(Object):
             connect_timeout: Timeout for client requests to haproxy in seconds.
             queue_timeout: Timeout for requests waiting in queue in seconds.
             server_maxconn: Maximum connections per server.
+            unit_address: IP address of the unit (if not provided, will use binding address).
         """
+        self._unit_address = unit_address
         self._application_data = self._generate_application_data(
             service,
             ports,

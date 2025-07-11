564a13b427d04654b7e4d88f203912c5741d6993
swetha1654 <swetha.swaminathan@canonical.com>
2025-07-11 18:14:06 +0530

    set status after start_server

--- tests/integration/any_charm_ingress_requirer.py
+++ tests/integration/any_charm_ingress_requirer.py
@@ -27,7 +27,6 @@ class AnyCharm(AnyCharmBase):  # pylint: disable=too-few-public-methods
         super().__init__(*args, **kwargs)
         self.ingress = IngressPerAppRequirer(self, port=80)
         self.unit.status = ops.BlockedStatus("Waiting for ingress relation")
-        self.framework.observe(self.on.ingress_relation_changed, self._on_ingress_relation_changed)
 
     def start_server(self):
         """Start apache2 webserver."""
@@ -41,7 +40,4 @@ class AnyCharm(AnyCharmBase):  # pylint: disable=too-few-public-methods
             "apache2",
         ]
         subprocess.run(install, capture_output=True, check=True)  # nosec
-
-    def _on_ingress_relation_changed(self, _: ops.ConfigChangedEvent):
-        """Relation changed handler."""
-        self.unit.status = ops.ActiveStatus()
+        self.unit.status = ops.ActiveStatus("Server Ready")

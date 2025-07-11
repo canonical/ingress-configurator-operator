f20450166cceebf3ad1b1336ed0493961948a71d
swetha1654 <swetha.swaminathan@canonical.com>
2025-07-11 14:28:28 +0530

    update file name

--- src/validators.py
+++ src/validators.py
@@ -1,7 +1,7 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""Helper methods for the state module."""
+"""Helper methods to validate the charm state."""
 
 import logging
 import re

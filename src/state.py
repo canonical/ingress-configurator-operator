e17533040da4b92c108a8fdab148aac89819d021
arturo-seijas <102022572+arturo-seijas@users.noreply.github.com>
2025-07-21 13:18:27 +0200

    Update src/state.py
    Co-authored-by: swetha1654 <swetha.swaminathan@canonical.com>

--- src/state.py
+++ src/state.py
@@ -95,7 +95,7 @@ class HealthCheck:
         all_or_none_health_checks_set = bool(interval) == bool(rise) == bool(fall)
         if not all_or_none_health_checks_set:
             raise InvalidStateError(
-                "Interval, raise and fall configurations need all to be defined if one is set."
+                "Health check configuration is incomplete: interval, rise, and fall must all be set if any one of them is specified."
             )
         return cls(interval=interval, rise=rise, fall=fall, path=path, port=port)
 

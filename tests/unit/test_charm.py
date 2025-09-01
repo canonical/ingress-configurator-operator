140f6079454035bccf4d45cea87c76c47ad28382
Fouad Atwi <fouad.atwi@canonical.com>
2025-09-01 17:53:14 +0200

    add: unit tests

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -9,6 +9,7 @@ import ops.testing
 import pytest
 
 import state
+from charm import IngressConfiguratorCharm
 
 
 def test_config_changed_no_haproxy_route_relation(context):
@@ -82,3 +83,93 @@ def test_protocol_propagated_to_haproxy(context: ops.testing.Context):
         "hosts": '["10.0.0.1"]',
         "protocol": '"https"',
     }
+
+
+class TestGetProxiedEndpointAction:
+    """Test "get-proxied-endpoints Action"""
+
+    @pytest.mark.parametrize(
+        "endpoints",
+        [
+            pytest.param(
+                '["https://fqdn.example/"]',
+                id="single_endpoint",
+            ),
+            pytest.param(
+                '["https://fqdn.example/", "https://fqdn2.example/"]',
+                id="multiple_endpoints",
+            ),
+        ],
+    )
+    def test_nominal(
+        self,
+        endpoints: str,
+        context: ops.testing.Context[IngressConfiguratorCharm],
+    ) -> None:
+        """
+        arrange: prepare state with haproxy relation
+        act: trigger a get-proxied-endpoint action.
+        assert: returns endpoint.
+        """
+        charm_state = ops.testing.State(
+            config={"backend-addresses": "10.0.0.1", "backend-ports": "80"},
+            relations=[
+                ops.testing.Relation(
+                    "haproxy-route",
+                    remote_app_data={"endpoints": endpoints},
+                ),
+            ],
+            leader=True,
+            unit_status=ops.testing.ActiveStatus(),
+        )
+        context.run(context.on.action("get-proxied-endpoints"), charm_state)
+
+        out = context.action_results
+
+        assert out == {"endpoints": endpoints}, "Unexpected action results."
+
+    def test_no_endpoints(
+        self,
+        context: ops.testing.Context[IngressConfiguratorCharm],
+    ) -> None:
+        """
+        arrange: prepare state with haproxy relation
+        act: trigger a get-proxied-endpoint action.
+        assert: returns endpoint.
+        """
+        charm_state = ops.testing.State(
+            config={"backend-addresses": "10.0.0.1", "backend-ports": "80"},
+            relations=[
+                ops.testing.Relation(
+                    "haproxy-route",
+                    remote_app_data={"endpoints": ""},
+                ),
+            ],
+            leader=True,
+            unit_status=ops.testing.ActiveStatus(),
+        )
+        context.run(context.on.action("get-proxied-endpoints"), charm_state)
+
+        out = context.action_results
+
+        assert out == {}, "Unexpected action results."
+
+    def test_no_haproxy_route_relation(
+        self,
+        context: ops.testing.Context[IngressConfiguratorCharm],
+    ) -> None:
+        """
+        arrange: prepare state with no haproxy relation.
+        act: trigger a get-proxied-endpoint action.
+        assert: Action returns empty dict.
+        """
+        charm_state = ops.testing.State(
+            config={"backend-addresses": "10.0.0.1", "backend-ports": "80"},
+            relations=[],
+            leader=True,
+            unit_status=ops.testing.ActiveStatus(),
+        )
+        context.run(context.on.action("get-proxied-endpoints"), charm_state)
+
+        out = context.action_results
+        assert out == {}

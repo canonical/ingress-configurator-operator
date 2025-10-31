61a590382d0eaa8e332c8837485e25c2e8637d59
tphan025 <trung.thanh.phan@canonical.com>
2025-10-31 23:56:14 +0100

    separate handling of relations

--- src/charm.py
+++ src/charm.py
@@ -52,48 +52,48 @@ class IngressConfiguratorCharm(ops.CharmBase):
     def _reconcile(self, _: ops.EventBase) -> None:
         """Refresh haproxy-route requirer data."""
         try:
-            if not self._haproxy_route.relation:
-                self.unit.status = ops.BlockedStatus("Missing haproxy-route relation.")
-                return
-            ingress_relation = self.model.get_relation(self._ingress.relation_name)
-            ingress_relation_data = (
-                self._ingress.get_data(ingress_relation) if ingress_relation else None
-            )
-            charm_state = State.from_charm(self, ingress_relation_data)
-            # Assign consistent_hashing to a local variable due to line length limit
-            consistent_hashing = charm_state.load_balancing_configuration.consistent_hashing
-            params = {
-                "hosts": [str(address) for address in charm_state.backend_addresses],
-                "check_interval": charm_state.health_check.interval,
-                "check_rise": charm_state.health_check.rise,
-                "check_fall": charm_state.health_check.fall,
-                "check_path": charm_state.health_check.path,
-                "check_port": charm_state.health_check.port,
-                "paths": charm_state.paths,
-                "ports": charm_state.backend_ports,
-                "protocol": charm_state.backend_protocol,
-                "retry_count": charm_state.retry.count,
-                "retry_redispatch": charm_state.retry.redispatch,
-                "server_timeout": charm_state.timeout.server,
-                "connect_timeout": charm_state.timeout.connect,
-                "queue_timeout": charm_state.timeout.queue,
-                "service": charm_state.service,
-                "hostname": charm_state.hostname,
-                "additional_hostnames": charm_state.additional_hostnames,
-                "load_balancing_algorithm": charm_state.load_balancing_configuration.algorithm,
-                "load_balancing_cookie": charm_state.load_balancing_configuration.cookie,
-                "load_balancing_consistent_hashing": consistent_hashing,
-                "http_server_close": charm_state.http_server_close,
-                "path_rewrite_expressions": charm_state.path_rewrite_expressions,
-            }
-            not_none_params = {k: v for k, v in params.items() if v is not None}
-            self._haproxy_route.provide_haproxy_route_requirements(**not_none_params)
-            proxied_endpoints = self._haproxy_route.get_proxied_endpoints()
-            if ingress_relation and proxied_endpoints:
-                self._ingress.publish_url(ingress_relation, str(proxied_endpoints[0]))
+            if self._haproxy_route.relation is not None:
+                ingress_relation = self.model.get_relation(self._ingress.relation_name)
+                ingress_relation_data = (
+                    self._ingress.get_data(ingress_relation) if ingress_relation else None
+                )
+                charm_state = State.from_charm(self, ingress_relation_data)
+                # Assign consistent_hashing to a local variable due to line length limit
+                consistent_hashing = charm_state.load_balancing_configuration.consistent_hashing
+                params = {
+                    "hosts": [str(address) for address in charm_state.backend_addresses],
+                    "check_interval": charm_state.health_check.interval,
+                    "check_rise": charm_state.health_check.rise,
+                    "check_fall": charm_state.health_check.fall,
+                    "check_path": charm_state.health_check.path,
+                    "check_port": charm_state.health_check.port,
+                    "paths": charm_state.paths,
+                    "ports": charm_state.backend_ports,
+                    "protocol": charm_state.backend_protocol,
+                    "retry_count": charm_state.retry.count,
+                    "retry_redispatch": charm_state.retry.redispatch,
+                    "server_timeout": charm_state.timeout.server,
+                    "connect_timeout": charm_state.timeout.connect,
+                    "queue_timeout": charm_state.timeout.queue,
+                    "service": charm_state.service,
+                    "hostname": charm_state.hostname,
+                    "additional_hostnames": charm_state.additional_hostnames,
+                    "load_balancing_algorithm": charm_state.load_balancing_configuration.algorithm,
+                    "load_balancing_cookie": charm_state.load_balancing_configuration.cookie,
+                    "load_balancing_consistent_hashing": consistent_hashing,
+                    "http_server_close": charm_state.http_server_close,
+                    "path_rewrite_expressions": charm_state.path_rewrite_expressions,
+                }
+                not_none_params = {k: v for k, v in params.items() if v is not None}
+                self._haproxy_route.provide_haproxy_route_requirements(**not_none_params)
+                proxied_endpoints = self._haproxy_route.get_proxied_endpoints()
+                if ingress_relation and proxied_endpoints:
+                    self._ingress.publish_url(ingress_relation, str(proxied_endpoints[0]))
 
-            # Handle TCP
-            self._provide_haproxy_route_tcp_requirements()
+            if self._haproxy_route_tcp.relation is not None:
+                # Handle TCP
+                self._provide_haproxy_route_tcp_requirements()
+
             self.unit.status = ops.ActiveStatus()
         except InvalidStateError as exc:
             logger.exception("Invalid configuration")

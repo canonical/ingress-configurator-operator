5d593105d44e8373a7afd03d84ad973e1a55a492
Arturo Seijas <arturo.seijas@canonical.com>
2026-03-25 12:35:09 +0100

    fix tests

--- tests/integration/prepare.sh
+++ tests/integration/prepare.sh
@@ -8,6 +8,5 @@ set -u
 # lxd should be install and init by a previous step in integration test action.
 echo "bootstrapping lxd juju controller"
 juju bootstrap localhost localhost
-# sudo k8s config | juju add-k8s ck8s --client
 juju add-cloud --controller localhost k8s
 juju add-model k8s k8s

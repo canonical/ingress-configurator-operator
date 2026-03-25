3f485a71c4277ebe808430ddd8ac9c1721ad6f85
Arturo Seijas <arturo.seijas@canonical.com>
2026-03-25 11:24:51 +0100

    fix tests

--- tests/integration/prepare.sh
+++ tests/integration/prepare.sh
@@ -8,4 +8,4 @@ set -euo pipefail
 # lxd should be install and init by a previous step in integration test action.
 echo "bootstrapping lxd juju controller"
 juju bootstrap localhost localhost
-sudo k8s config | juju add-k8s ck8s --client
+juju add-model k8s k8s

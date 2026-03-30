b635bb0f3bfc8355b4fb7fb875d18e23d0f76061
arturo-seijas <102022572+arturo-seijas@users.noreply.github.com>
2026-03-30 10:59:09 +0200

    Update src/kubernetes.py
    Co-authored-by: Phan Trung Thanh <trung.thanh.phan@canonical.com>

--- src/kubernetes.py
+++ src/kubernetes.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Canonical Ltd.
+# Copyright 2026 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 """Kubernetes helper methods for interacting with the cluster via lightkube."""

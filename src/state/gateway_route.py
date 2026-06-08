c05909466a612faa353be568cbb4c03d642fc411
Fouad Atwi <fouad.atwi@canonical.com>
2026-06-08 17:48:09 +0200

    Refactor GatewayRouteState to use default_factory for additional_hostnames and paths

--- src/state/gateway_route.py
+++ src/state/gateway_route.py
@@ -40,8 +40,8 @@ class GatewayRouteState:
     model_name: str
     hostname: Annotated[str, BeforeValidator(valid_fqdn)]
     port: int = Field(gt=0, le=65535)
-    additional_hostnames: list[Annotated[str, BeforeValidator(valid_fqdn)]] = Field(default=[])
-    paths: list[str] = Field(default=["/"])
+    additional_hostnames: list[Annotated[str, BeforeValidator(valid_fqdn)]] = Field(default_factory=list)
+    paths: list[str] = Field(default_factory=lambda: ["/"])
 
     @classmethod
     def build_for_adapter_mode(

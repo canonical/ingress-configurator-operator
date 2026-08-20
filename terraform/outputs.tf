43864399e3f5be021f37d0774e4d34da166ea82f
Fouad Atwi <fouad.atwi@canonical.com>
2026-08-20 17:26:46 +0200

    Revert "readd app_name"
    This reverts commit f81c9f3f3e54dd351911a273fbb42bfd9ff06b41.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,6 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-output "app_name" {
-  description = "Name of the deployed application. Deprecated: use application.name."
-  value       = juju_application.ingress-configurator.name
-}
-
 output "application" {
   description = "The deployed ingress-configurator application."
   value       = juju_application.ingress-configurator
@@ -15,10 +10,9 @@ output "provides" {
   description = "Map of the provided integration endpoints."
   value = {
     ingress = {
-      kind       = "endpoint"
-      name       = juju_application.ingress-configurator.name
-      endpoint   = "ingress"
-      controller = null
+      kind     = "endpoint"
+      name     = juju_application.ingress-configurator.name
+      endpoint = "ingress"
     }
   }
 }
@@ -27,22 +21,19 @@ output "requires" {
   description = "Map of the required integration endpoints."
   value = {
     gateway_route = {
-      kind       = "endpoint"
-      name       = juju_application.ingress-configurator.name
-      endpoint   = "gateway-route"
-      controller = null
+      kind     = "endpoint"
+      name     = juju_application.ingress-configurator.name
+      endpoint = "gateway-route"
     }
     haproxy_route = {
-      kind       = "endpoint"
-      name       = juju_application.ingress-configurator.name
-      endpoint   = "haproxy-route"
-      controller = null
+      kind     = "endpoint"
+      name     = juju_application.ingress-configurator.name
+      endpoint = "haproxy-route"
     }
     haproxy_route_tcp = {
-      kind       = "endpoint"
-      name       = juju_application.ingress-configurator.name
-      endpoint   = "haproxy-route-tcp"
-      controller = null
+      kind     = "endpoint"
+      name     = juju_application.ingress-configurator.name
+      endpoint = "haproxy-route-tcp"
     }
   }
 }

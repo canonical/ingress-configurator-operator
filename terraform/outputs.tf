f81c9f3f3e54dd351911a273fbb42bfd9ff06b41
Fouad Atwi <fouad.atwi@canonical.com>
2026-08-20 16:58:56 +0200

    readd app_name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,11 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+output "app_name" {
+  description = "Name of the deployed application. Deprecated: use application.name."
+  value       = juju_application.ingress-configurator.name
+}
+
 output "application" {
   description = "The deployed ingress-configurator application."
   value       = juju_application.ingress-configurator
@@ -10,9 +15,10 @@ output "provides" {
   description = "Map of the provided integration endpoints."
   value = {
     ingress = {
-      kind     = "endpoint"
-      name     = juju_application.ingress-configurator.name
-      endpoint = "ingress"
+      kind       = "endpoint"
+      name       = juju_application.ingress-configurator.name
+      endpoint   = "ingress"
+      controller = null
     }
   }
 }
@@ -21,19 +27,22 @@ output "requires" {
   description = "Map of the required integration endpoints."
   value = {
     gateway_route = {
-      kind     = "endpoint"
-      name     = juju_application.ingress-configurator.name
-      endpoint = "gateway-route"
+      kind       = "endpoint"
+      name       = juju_application.ingress-configurator.name
+      endpoint   = "gateway-route"
+      controller = null
     }
     haproxy_route = {
-      kind     = "endpoint"
-      name     = juju_application.ingress-configurator.name
-      endpoint = "haproxy-route"
+      kind       = "endpoint"
+      name       = juju_application.ingress-configurator.name
+      endpoint   = "haproxy-route"
+      controller = null
     }
     haproxy_route_tcp = {
-      kind     = "endpoint"
-      name     = juju_application.ingress-configurator.name
-      endpoint = "haproxy-route-tcp"
+      kind       = "endpoint"
+      name       = juju_application.ingress-configurator.name
+      endpoint   = "haproxy-route-tcp"
+      controller = null
     }
   }
 }
